937d81b5ad8c42f4785e305f0c821a56d10e1f17
Hector Mosquera <hmosquera@carvajalonline.com>
2019-02-08 12:07:28 -0500

    Subscriptions and local maven.

--- src/main/java/com/test/Client.java
+++ src/main/java/com/test/Client.java
@@ -1,5 +1,8 @@
 package com.test;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.context.annotation.Bean;
@@ -8,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.apollographql.apollo.ApolloClient;
 import com.apollographql.apollo.response.CustomTypeAdapter;
 import com.apollographql.apollo.response.CustomTypeValue;
+import com.apollographql.apollo.subscription.WebSocketSubscriptionTransport;
 import com.test.type.CustomType;
 
 import okhttp3.OkHttpClient;
@@ -15,18 +19,22 @@ import okhttp3.OkHttpClient;
 @Configuration
 public class Client {
 
-    private static final String BASE_URL = "http://localhost:8063/graphql";
+    private static final String BASE_URL = "http://localhost:8063/coordinator/register";
     
     private static final String SUBSCRIPTION_BASE_URL = "ws://localhost:8063/subscriptions";
 
     private CustomTypeAdapter<UUID> uuidCustomTypeAdapter;
+    private CustomTypeAdapter<LocalDateTime> localDateTimeCustomTypeAdapter;
+    private CustomTypeAdapter<BigDecimal> bigDecimalCustomTypeAdapter;
+    private CustomTypeAdapter<Instant> instantCustomTypeAdapter;
 
     @Bean
     public ApolloClient apolloClient() {
 
     	// UUID type adapter
         uuidCustomTypeAdapter = new CustomTypeAdapter<UUID>() {
-            @Override public UUID decode(
+            @Override
+            public UUID decode(
                 @SuppressWarnings("rawtypes")
                 CustomTypeValue value
             ) {
@@ -34,10 +42,65 @@ public class Client {
             }
 
             @SuppressWarnings("rawtypes")
-            @Override public CustomTypeValue encode(UUID value) {
+            @Override
+            public CustomTypeValue encode(UUID value) {
                 return new CustomTypeValue.GraphQLString(value.toString());
             }
         };
+
+    	// LocalDateTime type adapter
+        localDateTimeCustomTypeAdapter = new CustomTypeAdapter<LocalDateTime>() {
+		    @Override
+		    public LocalDateTime decode(
+		        @SuppressWarnings("rawtypes")
+		        CustomTypeValue value
+		    ) {
+//		        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+		        return LocalDateTime.parse(value.value.toString());
+			}
+
+			@SuppressWarnings("rawtypes")
+			@Override
+			public CustomTypeValue encode(LocalDateTime value) {
+                return new CustomTypeValue.GraphQLString(value.toString());
+			}
+        };
+
+    	// BigDecimal type adapter
+        bigDecimalCustomTypeAdapter = new CustomTypeAdapter<BigDecimal>() {
+		    @Override
+		    public BigDecimal decode(
+		        @SuppressWarnings("rawtypes")
+		        CustomTypeValue value
+		    ) {
+		        return new BigDecimal(value.value.toString());
+			}
+
+			@SuppressWarnings("rawtypes")
+			@Override
+			public CustomTypeValue encode(BigDecimal value) {
+                return new CustomTypeValue.GraphQLString(value.toString());
+			}
+        };
+
+    	// BigDecimal type adapter Instant
+        instantCustomTypeAdapter = new CustomTypeAdapter<Instant>() {
+		    @Override
+		    public Instant decode(
+		        @SuppressWarnings("rawtypes")
+		        CustomTypeValue value
+		    ) {
+//		        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+		        return Instant.parse(value.value.toString());
+			}
+
+			@SuppressWarnings("rawtypes")
+			@Override
+			public CustomTypeValue encode(Instant value) {
+                return new CustomTypeValue.GraphQLString(value.toString());
+			}
+        };
+
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
             //TODO add logging
@@ -48,8 +111,12 @@ public class Client {
             .serverUrl(BASE_URL)
             .okHttpClient(okHttpClient)
             .addCustomTypeAdapter(CustomType.UUID, uuidCustomTypeAdapter)
+            .addCustomTypeAdapter(CustomType.LOCALDATETIME, localDateTimeCustomTypeAdapter)
+            .addCustomTypeAdapter(CustomType.BIGDECIMAL, bigDecimalCustomTypeAdapter)
+            .addCustomTypeAdapter(CustomType.INSTANT, instantCustomTypeAdapter)
             //TODO add cache
 //            .normalizedCache(normalizedCacheFactory, cacheKeyResolver)
+            .subscriptionTransportFactory(new WebSocketSubscriptionTransport.Factory(SUBSCRIPTION_BASE_URL, okHttpClient))
             .build();
     }
 }
